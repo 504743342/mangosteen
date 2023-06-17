b366268e1baaab8b824f7dc8493732fe0b1bffe4
504743342 <504743342@qq.com>
2023-06-17 21:28:03 +0800

    router:重定向路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,4 @@
 import { RouteRecordRaw } from "vue-router";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { first } from "../components/welcome/first";
 import { second } from "../components/welcome/second";
 import { third } from "../components/welcome/third";
@@ -8,12 +6,12 @@ import { forth } from "../components/welcome/forth";
 import { welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-    { path: '/', component: Bar },
-    { path: '/about', component: Foo },
+    { path: '/', redirect: '/welcome'},
     { 
         path: '/welcome',
         component: welcome,  
         children: [
+            {path: '', redirect: '/welcome/1'},
             {path: '1', component: first},
             {path: '2', component: second},
             {path: '3', component: third},
--- src/views/Bar.tsx
+++
@@ -1,6 +0,0 @@
-import { defineComponent } from 'vue';
-export const Bar = defineComponent({
-setup: (props, content) => {
-return () => (<div>Bar</div>)
-}
-})
