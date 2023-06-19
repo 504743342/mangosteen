2aa684fb583e413e634b43824e42c62fd8898adb
504743342 <504743342@qq.com>
2023-06-19 18:06:08 +0800

    对 WelcomeLayout 再次重构

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,21 +1,22 @@
-import {WelcomeLayout} from './WelcomeLayout';
-import pig from '../../assets/icons/pig.svg';
-import { defineComponent } from "vue";
-import { RouterLink } from 'vue-router';
-import s from './WelcomeLayout.module.scss';
-export const First = defineComponent({
-  setup: (props, context) => {
-    const slots = {
-      icon: () => <img src={pig} />,
-      title: () => <h2>会挣钱 <br /> 还要会省钱</h2>,
-      buttons: () => <>
-         <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+import { WelcomeLayout } from "./WelcomeLayout";
+import pig from "../../assets/icons/pig.svg";
+import { RouterLink } from "vue-router";
+import s from "./WelcomeLayout.module.scss";
+export const First = () => {
+  return( 
+    <WelcomeLayout>
+      {{
+        icon: () => <img src={pig} />,
+        title: () => (
+          <h2>会挣钱 <br /> 还要会省钱</h2>
+        ),
+        buttons: () => <>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
           <RouterLink to="/welcome/2">下一页</RouterLink>
           <RouterLink to="/start">跳过</RouterLink>
-      </>
-    }
-    return () => (
-       <WelcomeLayout v-slots={slots}></WelcomeLayout>
-    );
-  },
-});
+        </>
+      }}
+    </WelcomeLayout>
+  )
+};
+First.displayName = 'First'
--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,15 +1,19 @@
-import { defineComponent } from "vue";
 import { RouterLink } from "vue-router";
-import s from './WelcomeLayout.module.scss';
-import cloud from '../../assets/icons/cloud.svg'
+import s from "./WelcomeLayout.module.scss";
+import cloud from "../../assets/icons/cloud.svg";
 import { WelcomeLayout } from "./WelcomeLayout";
-export const Forth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <WelcomeLayout>
+export const Forth = () => {
+  return (
+    <WelcomeLayout>
       {{
         icon: () => <img src={cloud} />,
-        title: () => <h2>云备份<br />再也不怕数据丢失</h2>,
+        title: () => (
+          <h2>
+            云备份
+            <br />
+            再也不怕数据丢失
+          </h2>
+        ),
         buttons: () => <>
           <RouterLink class={s.fake} to="/start">跳过</RouterLink>
           <RouterLink to="/start">开启应用</RouterLink>
@@ -17,6 +21,6 @@ export const Forth = defineComponent({
         </>
       }}
     </WelcomeLayout>
-    );
-  },
-});
+  );
+};
+Forth.displayName = 'Forth'
