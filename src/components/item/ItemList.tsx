efec68781107cccfbc77737d73b4f7f7d5717564
504743342 <504743342@qq.com>
2023-06-27 22:51:21 +0800

    重构 time 函数成 Time 类

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -3,6 +3,7 @@ import s from "./ItemList.module.scss";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Tab, Tabs } from "../../shared/Tabs";
+import { ItemSummary } from "./ItemSummary";
 export const ItemList = defineComponent({
   props: {
     name: {
@@ -17,7 +18,7 @@ export const ItemList = defineComponent({
         icon: () => <Icon name="menu"/>,
         default: () => (
           <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
-            <Tab name="本月">last1</Tab>
+            <Tab name="本月"><ItemSummary startDate="2000-01-01" endDate="2000-12-31"/></Tab>
             <Tab name="上月">last2</Tab>
             <Tab name="今年">last3</Tab>
             <Tab name="自定义时间">last4</Tab>
