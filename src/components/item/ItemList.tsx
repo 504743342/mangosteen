eadf23f94ae015ab5612b03597abf615f6b6ab8b
504743342 <504743342@qq.com>
2023-07-25 15:04:41 +0800

    选择开始时间

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,64 +1,83 @@
-import { defineComponent, reactive, ref } from "vue";
-import { MainLayout } from "../../layouts/MainLayout";
-import { Icon } from "../../shared/Icon";
-import { Tab, Tabs } from "../../shared/Tabs";
-import { ItemSummary } from "./ItemSummary";
-import { Time } from "../../shared/time";
+import { Overlay } from 'vant';
+import { defineComponent, PropType, reactive, ref, watchEffect } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Form, FormItem } from '../../shared/Form';
+import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
+import { Time } from '../../shared/time';
+import s from './ItemList.module.scss';
+import { ItemSummary } from './ItemSummary';
 export const ItemList = defineComponent({
   setup: (props, context) => {
     const refSelected = ref('本月')
     const time = new Time()
     const customTime = reactive({
-      start: new Time(), 
-      end: new Time()
+      start: new Time().format(),
+      end: new Time().format()
     })
-    const timeList = [ 
+    const timeList = [
       {
-        start: time.firstDayOfMonth(), 
+        start: time.firstDayOfMonth(),
         end: time.lastDayOfMonth()
       },
       {
-        start: time.add(-1, 'month').firstDayOfMonth(), 
+        start: time.add(-1, 'month').firstDayOfMonth(),
         end: time.add(-1, 'month').lastDayOfMonth()
       },
       {
         start: time.firstDayOfYear(),
         end: time.lastDayOfYear()
-      },
+      }
     ]
+    watchEffect(() => {
+      if (refSelected.value === '自定义时间') {
+        refOverlayVisible.value = true
+      }
+    })
+    const refOverlayVisible = ref(false)
     return () => (
-      <MainLayout>{{
-        title: () => "山竹记账",
-        icon: () => <Icon name="menu"/>,
-        default: () => (
-          <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
-            <Tab name="本月">
-              <ItemSummary 
-                startDate={timeList[0].start.format()} 
-                endDate={timeList[0].end.format()}
-              />
-            </Tab>
-            <Tab name="上月">
-            <ItemSummary 
-                startDate={timeList[1].start.format()} 
-                endDate={timeList[1].end.format()}
-              />
-            </Tab>
-            <Tab name="今年">
-            <ItemSummary 
-                startDate={timeList[2].start.format()} 
-                endDate={timeList[2].end.format()}
-              />
-            </Tab>
-            <Tab name="自定义时间">
-            <ItemSummary 
-                startDate={timeList[3].start.format()} 
-                endDate={timeList[3].end.format()}
-              />
-            </Tab>
-          </Tabs>
-        ),
-      }}</MainLayout>
-    );
-  },
-});
+      <MainLayout>{
+        {
+          title: () => '山竹记账',
+          icon: () => <Icon name="menu" />,
+          default: () => <>
+            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+              <Tab name="本月">
+                <ItemSummary
+                  startDate={timeList[0].start.format()}
+                  endDate={timeList[0].end.format()} />
+              </Tab>
+              <Tab name="上月">
+                <ItemSummary
+                  startDate={timeList[1].start.format()}
+                  endDate={timeList[1].end.format()} />
+              </Tab>
+              <Tab name="今年">
+                <ItemSummary
+                  startDate={timeList[2].start.format()}
+                  endDate={timeList[2].end.format()} />
+              </Tab>
+              <Tab name="自定义时间">
+                <ItemSummary
+                  startDate={customTime.start}
+                  endDate={customTime.end} />
+              </Tab>
+            </Tabs>
+            <Overlay show={refOverlayVisible.value} class={s.overlay} >
+              <div class={s.overlay_inner}>
+                <header>
+                  请选择时间
+                </header>
+                <main>
+                  <Form>
+                    <FormItem label='开始时间' v-model={customTime.start} type='date' />
+                  </Form>
+                </main>
+              </div>
+            </Overlay>
+          </>
+        }
+      }</MainLayout>
+    )
+  }
+})
