ab7cfff96521d6e4d661b353958aad5272d8e1b6
504743342 <504743342@qq.com>
2023-07-31 16:39:19 +0800

    创建统计图表页面

---
+++ src/views/StatisticsPage.tsx
@@ -0,0 +1,16 @@
+import { PropType, defineComponent } from "vue";
+import s from "./StatisticsPage.module.scss";
+import { TimeTabsLayout } from "../layouts/TimeTabsLayout";
+import { Charts } from "../components/statistics/Charts";
+export const StatisticsPage = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>,
+    },
+  },
+  setup: (props, context) => {
+    return () => (
+        <TimeTabsLayout component={Charts} />
+    );
+  },
+});
