3c88f5c36098629f3fcbe0b03410158df5fc5148
504743342 <504743342@qq.com>
2023-08-08 19:18:49 +0800

    创建 mock 模块

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,47 +1,73 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
-type JSONValue = string | number | null | boolean | JSONValue[] | { [key: string]: JSONValue }
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { mockSession } from "../mock/mock";
+
+type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
+type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
+type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>
+type DeleteConfig = Omit<AxiosRequestConfig, 'params'>
 
 export class Http {
-    instance: AxiosInstance;
-    constructor(baseURL: string) {
-        this.instance = axios.create({
-            baseURL
-        })
-    }
-    get<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>){
-        return this.instance.request<R>({...config, url, params: query, method: 'get'})
-    }
-    post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'data' | 'url' | 'method'>){
-        return this.instance.request<R>({...config, url, data, method: 'post'})
-    }
-    patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data'>){
-        return this.instance.request<R>({...config, url, data, method: 'patch'})
-    }
-    delete<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig, 'params'>){
-        return this.instance.request<R>({...config, url, params: query, method: 'delete'})
-    }
+  instance: AxiosInstance
+  constructor(baseURL: string) {
+    this.instance = axios.create({
+      baseURL
+    })
+  }
+  get<R = unknown>(url: string, query?: Record<string, JSONValue>, config?: GetConfig) {
+    return this.instance.request<R>({ ...config, url: url, params: query, method: 'get' })
+  }
+  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PostConfig) {
+    return this.instance.request<R>({ ...config, url, data, method: 'post' })
+  }
+  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PatchConfig) {
+    return this.instance.request<R>({ ...config, url, data, method: 'patch' })
+  }
+  delete<R = unknown>(url: string, query?: Record<string, string>, config?: DeleteConfig) {
+    return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
+  }
+}
+
+const mock = (response: AxiosResponse) => {
+  if (location.hostname !== 'localhost'
+    && location.hostname !== '127.0.0.1'
+    && location.hostname !== '192.168.3.57') { return false }
+  switch (response.config?.params?._mock) {
+    case 'session':
+      [response.status, response.data] = mockSession(response.config)
+      return true
+  }
+  return false
 }
 
 export const http = new Http('/api/v1')
 
 http.instance.interceptors.request.use(config => {
-    const jwt = localStorage.getItem('jwt')
-    if(jwt){
-        config.headers!.Authorization = `Bearer ${jwt}`
-    }
-    return config
+  const jwt = localStorage.getItem('jwt')
+  if (jwt) {
+    config.headers!.Authorization = `Bearer ${jwt}`
+  }
+  return config
 })
 
-http.instance.interceptors.response.use(response => {
-    return response
-}, error => {
-    if (error.response){
-        const axiosError = error as AxiosError
-        if(axiosError.response?.status === 429){
-            alert('请求过于频繁，请稍后再试')
-        }
-
+http.instance.interceptors.response.use((response) => {
+  mock(response)
+  return response
+}, (error) => {
+  if (mock(error.response)) {
+    return error.response
+  } else {
+    throw error
+  }
+})
+http.instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response) {
+      const axiosError = error as AxiosError
+      if (axiosError.response?.status === 429) {
+        alert('你太频繁了')
+      }
     }
-        throw error
-    }
+    throw error
+  }
 )
