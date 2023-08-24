2eae4658ad1364328b5f929fd3a348c851450ce0
504743342 <504743342@qq.com>
2023-08-24 19:37:19 +0800

    初步转换折线图数据

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -4,7 +4,7 @@ import axios, {
   AxiosRequestConfig,
   AxiosResponse,
 } from "axios";
-import { mockItemCreate, mockItemIndex, mockItemIndexBalance, mockSession, mockTagEdit, mockTagIndex, mockTagShow } from "../mock/mock";
+import { mockItemCreate, mockItemIndex, mockItemIndexBalance, mockItemSummary, mockSession, mockTagEdit, mockTagIndex, mockTagShow } from "../mock/mock";
 
 type GetConfig = Omit<AxiosRequestConfig, "params" | "url" | "method">;
 type PostConfig = Omit<AxiosRequestConfig, "url" | "data" | "method">;
@@ -87,6 +87,9 @@ const mock = (response: AxiosResponse) => {
       return true
     case 'itemIndexBalance': 
       [response.status, response.data] = mockItemIndexBalance(response.config)
+      return true
+    case 'itemSummary': 
+      [response.status, response.data] = mockItemSummary(response.config)
       return true
   }
   return false;
