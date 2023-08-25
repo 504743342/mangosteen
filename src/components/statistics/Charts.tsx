42ab2b2283bceb0c85df39caa526d4228e011f8f
504743342 <504743342@qq.com>
2023-08-25 18:33:13 +0800

    完成 bar chart

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -69,6 +69,15 @@ export const Charts = defineComponent({
       })
       data2.value = response.data.groups
     })
+
+    //data3
+    const betterData3 = computed(() => {
+      const total = data2.value.reduce((sum, item) => sum + item.amount, 0);
+      return data2.value.map((item) => ({
+        ...item,
+        percent: Math.round((item.amount / total) * 100)
+      }));
+    });
     return () => (
         <div class={s.wrapper}>
             <FormItem label='类型' type="select" options={[
@@ -77,7 +86,7 @@ export const Charts = defineComponent({
             ]} v-model={kind.value}/>
             <LineChart data={betterData1.value}/>
             <PieChart data={betterData2.value}/>
-            <Bars />
+            <Bars data={betterData3.value}/>
         </div>  
     )
   },
