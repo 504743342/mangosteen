514ea900f88ebab4fbe5b8e615da2e7af22c1450
504743342 <504743342@qq.com>
2023-08-30 21:04:14 +0800

    合并 /start 和 /items

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -8,7 +8,6 @@ import { ThirdActions } from "../components/welcome/ThirdActions";
 import { Forth } from "../components/welcome/Forth";
 import { ForthActions } from "../components/welcome/ForthActions";
 import { Welcome } from "../views/Welcome";
-import { StartPage } from "../views/StartPage";
 import { ItemPage } from "../views/ItemPage";
 import { ItemList } from "../components/item/ItemList";
 import { ItemCreate } from "../components/item/ItemCreate";
@@ -24,7 +23,7 @@ export const routes: RouteRecordRaw[] = [
         path: '/welcome',
         component: Welcome,  
         beforeEnter: (to, from, next) => {
-            localStorage.getItem('skipFeatures') ? next('/start') : next()
+            localStorage.getItem('skipFeatures') ? next('/items') : next()
         },
         children: [
             {path: '', redirect: '/welcome/1'},
@@ -34,7 +33,6 @@ export const routes: RouteRecordRaw[] = [
             {path: '4', name: "Welcome4", components: {main: Forth, footer: ForthActions}}
         ]
     },
-    {path: '/start', component: StartPage},
     {
         path: '/items', 
         component: ItemPage,
